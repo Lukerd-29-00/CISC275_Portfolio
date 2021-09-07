46084a1cd96d4a2454f5317b9a9274ba2b38b76d
lucasd <Driscoll_Lucas@bah.com>
2021-09-07 18:01:01 -0400

    added window title. Seriously, this time.

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-document.title = "Lucas's Portfolio"
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
