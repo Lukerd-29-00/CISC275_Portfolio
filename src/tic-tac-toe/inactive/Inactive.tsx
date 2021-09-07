eb470a6c8ed151f4bcf96609b33818748253ab6a
lucasd <Driscoll_Lucas@bah.com>
2021-09-07 18:11:03 -0400

    updated tic-tac-toe description

--- src/tic-tac-toe/inactive/Inactive.tsx
+++ src/tic-tac-toe/inactive/Inactive.tsx
@@ -4,7 +4,7 @@ export function Inactive(props: inactiveProps){
     return (
     <div>
         <h2><button className="link" onClick={props.enabler}>Tic-Tac-Toe</button></h2>
-        <p>This is based on this <a href="https://reactjs.org/tutorial/tutorial.html">React tutorial</a>, but it uses hooks and function components instead of the class-based approach. The stying is copied directly from the tutorial.</p>
+        <p>This is based on this <a href="https://reactjs.org/tutorial/tutorial.html">React tutorial</a>, but it uses hooks and function components instead of the class-based approach. The stying is a slight modification of the css presented in that tutorial.</p>
     </div>
     )
 }
