140211ad4d55819831f88cf7c66cc700e59fcd93
lucasd <Driscoll_Lucas@bah.com>
2021-09-23 14:06:16 -0400

    Added kings that can move in both directions. Kings are marked with a star. Fixed some minor movement issues.

--- src/Checkers/components/CheckersBoard.tsx
+++ src/Checkers/components/CheckersBoard.tsx
@@ -23,6 +23,7 @@ export interface Square{
 export interface Move {
     deletes: Square | null
     destination: Square
+    endsTurn: boolean
 }
 
 interface NoProps {
@@ -60,11 +61,17 @@ function getAdjacentSquares(square: Square, board: Array<Array<Square>>, piece?:
 }
 
 function getAdjacentEnemyPieces(square: Square | null,board: Array<Array<Square>>,piece?: Piece): Array<Square>{
-    if(square === null || square.piece === null){
+    if(square === null){
         return [];
     }
+    else if(square.piece === null && piece === undefined){
+        throw Error("No piece to move!")
+    }
+    else if(piece === undefined){
+        piece = (square.piece as Piece);
+    }
     return getAdjacentSquares(square,board,piece).filter((sq: Square) => {
-        return sq.piece !== null && sq.piece.color !== (square.piece as Piece).color
+        return sq.piece !== null && sq.piece.color !== (piece as Piece).color
     })
 }
 
@@ -77,6 +84,7 @@ function getAdjacentEmptySquares(square: Square | null, board: Array<Array<Squar
     })
 }
 
+
 export function CheckersBoard(props: NoProps){
     const [squares, movePiece, setSquaresHighlighted] = useSquares();
     const [selectedSquare, setSelectedSquare] = useState<Square | null>(null);
@@ -84,6 +92,7 @@ export function CheckersBoard(props: NoProps){
     const [isFirstMove, setIsFirstMove] = useState(true);
     const [selectedPiece, selectPiece] = useState<Piece | null>(null)
     const [startingSquare, setStartingSquare] = useState<Square | null>(null);
+    const [winner, setWinner] = useState<string | null>(null);
     const selectSquare = (sq: Square | null) => {
         let allSquares = new Array<Square>(64);
         for(let i = 0;i < 64;i++){
@@ -92,36 +101,51 @@ export function CheckersBoard(props: NoProps){
         setSquaresHighlighted(allSquares,false);
         setSelectedSquare(sq);
     }
+    const endTurn = (redTurn: boolean) => {
+        setRedsTurn(!redTurn);
+        setIsFirstMove(true);
+        selectSquare(null);
+        setStartingSquare(null)
+        selectPiece(null);
+        let allSquares = new Array<Square>(64);
+        for(let i = 0;i < 64;i++){
+            allSquares[i] = squares[Math.floor(i/8)][i % 8]
+        }
+        setSquaresHighlighted(allSquares,false);
+    }
     let moves = new Array<Move>();
     if(selectedSquare !== null && selectedPiece !== null && startingSquare !== null){
 
         //if this is the first move of the turn, allow moving into nearby empty spaces.
         if(isFirstMove || selectedSquare.piece !== selectedPiece){
             moves = moves.concat(getAdjacentEmptySquares(selectedSquare,squares,selectedPiece).map((square :Square) => {
-                return {destination: square,deletes: (selectedSquare.piece as Piece).color !== selectedPiece.color ? selectedSquare : null}
-            }))
+                return {destination: square,deletes: (selectedSquare.piece as Piece).color !== selectedPiece.color ? selectedSquare : null, endsTurn: selectedSquare.piece === selectedPiece}
+            })).filter((move: Move) => {
+                return move.destination.position.row !== startingSquare.position.row || move.destination.position.col !== startingSquare.position.col
+            })
         }
-        //Add any squares that kill nearby enemies.
-        moves = moves.concat(getAdjacentEnemyPieces(selectedSquare,squares,selectedPiece).filter((sq: Square) => {
-            return getAdjacentEmptySquares(sq,squares,selectedPiece).length > 0
-        }).map((place: Square) => {
-                return {destination: place,deletes: null}
-        }));
+        if(selectedSquare.piece === selectedPiece || selectedSquare.piece === null){
+            //Add any squares that kill nearby enemies.
+            moves = moves.concat(getAdjacentEnemyPieces(selectedSquare,squares,selectedPiece).filter((sq: Square) => {
+                return (getAdjacentEmptySquares(sq,squares,selectedPiece)).filter((empty: Square) => {
+                    return empty.position.row !== selectedSquare.position.row || empty.position.col !== selectedSquare.position.col;
+                }).length > 0
+            }).map((place: Square) => {
+                    return {destination: place,deletes: null, endsTurn: false}
+            }));
+        }
+
+
         
         //Currently breaks in a case where two different moves have the same destination; instead of highlighting destinations, it should highlight the piece to delete then let you select a square from that piece.
         //If no moves can be made, the other player gets their turn.
-
-        if(moves.length === 0){
-            setRedsTurn(!redsTurn);
-            setIsFirstMove(true);
+        if(moves.length === 0 && isFirstMove){
+            setStartingSquare(null);
             selectSquare(null);
-            setStartingSquare(null)
             selectPiece(null);
-            let allSquares = new Array<Square>(64);
-            for(let i = 0;i < 64;i++){
-                allSquares[i] = squares[Math.floor(i/8)][i % 8]
-            }
-            setSquaresHighlighted(allSquares,false);
+        }
+        else if(moves.length === 0){
+            endTurn(redsTurn);
         }
         else{
             for(const move of moves){
@@ -149,15 +173,16 @@ export function CheckersBoard(props: NoProps){
                                 selectedPiece: selectedPiece,
                                 startingSquare: startingSquare,
                                 selectStartingSquare: setStartingSquare,
+                                endTurn: () => {endTurn(redsTurn)},
                                 child: <></>    
                             }
     
                             if(square.piece !== null && square.piece.color === "black"){
-                                output.child = <span className="dot" role="black-piece"/>
+                                output.child = <span className="piece" role="black-piece">{square.piece.king ? "*" : ""}</span>
                                 return CheckersSquare(output);
                             }
                             else if(square.piece !== null && square.piece.color === "red"){
-                                output.child = <span className="dot red" role="red-piece"/>
+                                output.child = <span className="piece red" role="red-piece">{square.piece.king ? "*" : ""}</span>
                                 return CheckersSquare(output);
                             }
                             return CheckersSquare(output);
--- src/Checkers/components/CheckersSquare.tsx
+++ src/Checkers/components/CheckersSquare.tsx
@@ -11,6 +11,7 @@ export interface SquareProps {
     selectStartingSquare: (square: Square | null) => void
     selectedPiece: Piece | null
     startingSquare: Square | null
+    endTurn: () => void
     child: JSX.Element
 }
 
@@ -30,14 +31,28 @@ export function CheckersSquare(props: SquareProps){
                     if(move.destination.piece === null){
                         props.movePiece(move);
                         props.setFirstMove(false);
-                        if(move.deletes !== null){
+                        if(move.destination.position.row === 0 && (props.selectedPiece as Piece).color === "red" && !(props.selectedPiece as Piece).king){
+                            const newPiece = {color: ("red" as "black" | "red"),king: true}
+                            const newSquare = {position: move.destination.position, color: move.destination.color, highlighted: move.destination.highlighted, piece: newPiece} 
+                            props.selectStartingSquare(newSquare)
+                            props.selectPiece(newPiece); 
+                            props.selectSquare(newSquare) 
+                        }
+                        else if(move.destination.position.row === 7 && (props.selectedPiece as Piece).color === "black" && !(props.selectedPiece as Piece).king){
+                            const newPiece = {color: ("black" as "black" | "red"),king: true}
+                            const newSquare = {position: move.destination.position, color: move.destination.color, highlighted: move.destination.highlighted, piece: newPiece} 
+                            props.selectStartingSquare(newSquare)
+                            props.selectPiece(newPiece); 
+                            props.selectSquare(newSquare) 
+                        }
+                        else{
                             props.selectSquare(move.destination);
                             props.selectStartingSquare(move.destination)
                         }
-                        if(move.destination.position.row === 0 && (props.selectedPiece as Piece).color === "red" && !(props.selectedPiece as Piece).king){
-                            props.selectStartingSquare({position: move.destination.position, color: move.destination.color, highlighted: move.destination.highlighted, piece: {color: "red",king: true}})
-                            props.selectPiece({color: "red",king: true});  
+                        if(move.endsTurn){
+                            props.endTurn();
                         }
+                        
                     }
                     else{
                         props.selectSquare(props.square)
